ac256f2dd5c1010d72105ed5c8a6b57998fd7c96
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-06-29 19:17:19 +0200

    Update deployment_setup.sh

--- deployment_setup.sh
+++ deployment_setup.sh
@@ -4,7 +4,8 @@ export LC_ALL=C
 export LC_CTYPE="UTF-8",
 export LANG="en_US.UTF-8"
 
-apt install -y python-jinja2 python-pip libssl-dev curl git vim
+DEBIAN_FRONTEND=noninteractive apt update
+DEBIAN_FRONTEND=noninteractive apt install -y python-jinja2 python-pip libssl-dev curl git vim
 pip install -U pip
 
 reboot
