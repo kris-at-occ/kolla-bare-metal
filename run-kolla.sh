53218e22847c0b724e2bb81d96a67da79f2373a2
kris-at-occ <kris@opencloudconsulting.net>
2018-07-01 15:21:26 +0200

    Adding examples directory

--- run-kolla.sh
+++ run-kolla.sh
@@ -4,56 +4,6 @@ export LC_ALL=C
 export LC_CTYPE="UTF-8",
 export LANG="en_US.UTF-8"
 
-echo 'run-kolla.sh: Cleaning directory /home/openstack/.ssh/'
-rm -f /home/openstack/.ssh/known_hosts
-rm -f /home/openstack/.ssh/id_rsa
-rm -f /home/openstack/.ssh/id_rsa.pub
-
-echo 'run-kolla.sh: Running ssh-keygen -t rsa'
-ssh-keygen -t rsa
-
-echo 'run-kolla.sh: Running ssh-copy-id openstack@controller1'
-ssh-copy-id openstack@controller1
-echo 'run-kolla.sh: Running ssh-copy-id openstack@controller2'
-ssh-copy-id openstack@controller2
-echo 'run-kolla.sh: Running ssh-copy-id openstack@compute001'
-ssh-copy-id openstack@compute001
-
-echo 'run-kolla.sh: Running scp controller_setup.sh openstack@controller1:/home/openstack/controller_setup.sh'
-scp controller_setup.sh openstack@controller1:/home/openstack/controller_setup.sh
-echo 'run-kolla.sh: Running scp controller_setup.sh openstack@controller2:/home/openstack/controller_setup.sh'
-scp controller_setup.sh openstack@controller2:/home/openstack/controller_setup.sh
-echo 'run-kolla.sh: Running scp compute_setup.sh openstack@compute001:/home/openstack/compute_setup.sh'
-scp compute_setup.sh openstack@compute001:/home/openstack/compute_setup.sh
-
-echo 'run-kolla.sh: Running ssh openstack@controller1 "sudo bash /home/openstack/controller_setup.sh"'
-ssh openstack@controller1 "sudo bash /home/openstack/controller_setup.sh"
-echo 'run-kolla.sh: Running ssh openstack@controller2 "sudo bash /home/openstack/controller_setup.sh"'
-ssh openstack@controller2 "sudo bash /home/openstack/controller_setup.sh"
-echo 'run-kolla.sh: Running ssh openstack@compute001 "sudo bash /home/openstack/compute_setup.sh"'
-ssh openstack@compute001 "sudo bash /home/openstack/compute_setup.sh"
-
-echo 'run-kolla.sh: Running sudo pip install ansible==2.5.2'
-sudo pip install ansible==2.5.2
-
-if [ $? -ne 0 ]; then
-  echo "Cannot install Ansible"
-  exit $?
-fi
-
-echo 'run-kolla.sh: Running sudo pip install kolla-ansible==6.0.0'
-sudo pip install kolla-ansible==6.0.0
-
-if [ $? -ne 0 ]; then
-  echo "Cannot install kolla-ansible"
-  exit $?
-fi
-
-echo 'run-kolla.sh: Running sudo cp -r /usr/local/share/kolla-ansible/etc_examples/kolla /etc/kolla'
-sudo cp -r /usr/local/share/kolla-ansible/etc_examples/kolla /etc/kolla
-echo 'run-kolla.sh: Running sudo cp globals.yml /etc/kolla'
-sudo cp globals.yml /etc/kolla
-
 export ANSIBLE_HOST_KEY_CHECKING=False
 
 echo 'run-kolla.sh: Running sudo kolla-genpwd'
